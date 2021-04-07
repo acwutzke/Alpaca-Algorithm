9badd969999db0b1f6874d6d7e102f8ded8480a3
Alex Wutzke <acharleswutzke@gmail.com>
2021-04-07 15:11:41 -0600

    add stock download using yfinance, added function to make predictions

--- functions.py
+++ functions.py
@@ -1,8 +1,14 @@
 import requests, json, datetime
 import yfinance as yf
 import pandas as pd
+import numpy as np
 pd.options.mode.chained_assignment = None
 import param
+
+import xgboost as xgb
+from xgboost import XGBClassifier
+import pickle
+from xgb_functions import *
 
 
 # get account information
@@ -62,7 +68,7 @@ def index_check():
   # sort dictionary
   ind={k: v for k, v in sorted(ind.items(), key=lambda item: item[1], reverse=True)}
   # take top three indexes
-  print("Top indexes over 50 day SMA")
+  print("Top indexes over 50 day SMA:")
   cntr=0
   topind={}
   for i in ind:
@@ -70,17 +76,6 @@ def index_check():
       topind[i]=ind[i]
     cntr+=1
   return topind
-
-
-  # data=data.reset_index()[['Date','Adj Close']]
-  # data[20]=data['Adj Close'].rolling(20).mean()
-  # 
-  # data['MACD']=data[20]/data[50]
-  # data['index']=data['Adj Close']/data[50]
-
-  # return data['index'].iloc[-1]
-
-
 
 # function to clean up yfinance output for pandas
 # takes a pandas dataframe 
@@ -122,3 +117,74 @@ def get_stock_groups(top_indexes):
         print(g)
         ls.append(g)
   return ls
+
+def check_market():
+  ct = datetime.datetime.today()+datetime.timedelta(days=1)
+  end = ct.strftime("%Y-%m-%d")
+  start=ct-datetime.timedelta(days=1)
+  start=start.strftime("%Y-%m-%d")
+  data = yf.download("SPY", start=start, end=end)
+  data=data.reset_index()
+  if data['Date'].iloc[0].strftime("%Y-%m-%d")==datetime.datetime.today().strftime("%Y-%m-%d"):
+    return True
+  return False
+
+def get_predictions(group):
+
+  ticker_file='models/'+group+'.csv'
+  ticker_list=get_samples(file=ticker_file,n='all')
+  print("downloading ticker list: ",ticker_list)
+
+  # download data from yfinance
+  ct = datetime.datetime.today()+timedelta(days=1)
+  end = ct.strftime("%Y-%m-%d")
+  start=ct-timedelta(days=120)
+  start=start.strftime("%Y-%m-%d")
+  prediction_data=get_data(ticker_list,exchange='',start=start,end=end)
+  print("Data download complete")
+  print("calculating technicals...")
+
+   
+  predictiondf=SMA(prediction_data,10)
+  predictiondf=FI(predictiondf,30)
+  predictiondf=RSI(predictiondf,14)
+  predictiondf=MACD(predictiondf,10,30)
+  predictiondf=MACD(predictiondf,5,10)
+  predictiondf=MACDiff(predictiondf,5)
+  predictiondf=index(predictiondf,index='SPY',days=30)
+  print("Technicals calculated")
+  print("Generating dataset for XGBoost...")
+
+  feat=['sma','rsi','fi','macd','index']
+  predictionset=generate_xgb_set(predictiondf,features=feat)
+  predictionset=predictionset.dropna()
+
+  # Based on correlation with the target these are observed to be the best features
+  best_feat=['_sma_10','_rsi_14','_fi_30','_macd_5_10','_macd_10_30_diff5','_index_SPY30']
+
+  # dump train and test sets into numpy arrays for training
+  x_prediction=predictionset[best_feat].to_numpy()
+  extra_prediction=predictionset[['Date','STOCK']].to_numpy()
+  print(x_prediction.shape)
+  print("Dataset for XGBoost prepared")
+  print("Loading model...")
+
+  model_name='models/'+group+'_model.pickle.dat'
+
+  try:
+    model = pickle.load(open(model_name, "rb"))
+  except:
+    print("Model not found, make sure the model exists in the models folder.")
+    exit()
+
+  print("Model loaded! making predictions...")
+  pred=model.predict_proba(x_prediction)
+  predyes=[]
+  for p in pred:
+    predyes.append(p[1])
+
+  resultdf=pd.DataFrame(extra_prediction)
+  resultdf.columns=['Date','Stock']
+  resultdf['prediction']=predyes
+  resultdf=resultdf.sort_values(by=['Date','prediction'], ascending=False).head(5)
+  print(resultdf)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # import libraries
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import datetime, timedelta
 import requests, json, websocket
 import yfinance as yf
+
+import xgboost as xgb
+from xgboost import XGBClassifier
+import pickle
+from xgb_functions import *
+
 
 # import functions
 from config import *
@@ -20,13 +26,51 @@ account_url="{}/v2/account".format(base_url)
 positions_url="{}/v2/positions".format(base_url)
 orders_url="{}/v2/orders".format(base_url)
 
+# check whether the market is open
+if check_market():
+	print("Market is open!")
+else:
+	print("Market is closed, exiting script")
+	exit()
+
 print("Checking indexes")
 top_indexes=index_check()
 
 print(top_indexes)
 
-#
-res=get_stock_groups(top_indexes)
-print(res)
+groups=get_stock_groups(top_indexes)
+
+# ticker_list=[]
+# for g in groups:
+# 	ticker_file='models/'+g+'.csv'
+# 	ls=get_samples(file=ticker_file,n=5)
+# 	ticker_list+=ls
+
+# print(len(ticker_list))
+
+# download data from yfinance
+# ct = datetime.datetime.today()+timedelta(days=1)
+# end = ct.strftime("%Y-%m-%d")
+# start=ct-timedelta(days=120)
+# start=start.strftime("%Y-%m-%d")
+# prediction_data=get_data(ticker_list,exchange='',start=start,end=end)
+# print("Data download complete, calculating technicals...")
+
+# calculate technical 
+# predictiondf=SMA(prediction_data,10)
+# predictiondf=FI(predictiondf,30)
+# predictiondf=RSI(predictiondf,14)
+# predictiondf=MACD(predictiondf,10,30)
+# predictiondf=MACD(predictiondf,5,10)
+# predictiondf=MACDiff(predictiondf,5)
+# predictiondf=index(predictiondf,index='SPY',days=30)
+
+group='US_METALS'
+
+get_predictions(group)
+
+
+
+
 
 
